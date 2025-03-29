1e7d52ad0bd2a0abd3de16213c5cd26c0d7a4b2b
STEpHANE <95184279+ZFEbHVUE@users.noreply.github.com>
2025-03-29 17:10:02 +0100

    Update AudioManager.cpp

--- es-core/src/AudioManager.cpp
+++ es-core/src/AudioManager.cpp
@@ -210,37 +210,38 @@ bool AudioManager::songWasPlayedRecently(const std::string& song)
 	return false;
 }
 
-void AudioManager::playRandomMusic(bool useFavorites) 
+void AudioManager::playRandomMusic(bool useFavorites)
 {
 	if (!Settings::BackgroundMusic())
 		return;
-		
+
 	std::vector<std::string> musics;
 
-	// Utiliser les favoris si demandé
-	std::string musicPath = Paths::getUserMusicPath();
-	std::string favoritesFile = musicPath + "/favorites.m3u";
-
-	if (useFavorites && Utils::FileSystem::exists(favoritesFile))
+	if (useFavorites)
 	{
-		auto lines = Utils::FileSystem::readAllLines(favoritesFile);
-		musics.assign(lines.begin(), lines.end());
+		std::string favoritesFile = Paths::getUserMusicPath() + "/favorites.m3u";
+
+		if (Utils::FileSystem::exists(favoritesFile))
+		{
+			auto lines = Utils::FileSystem::readAllLines(favoritesFile);
+			for (const auto& line : lines)
+			{
+				if (!line.empty())
+					musics.push_back(line);
+			}
+		}
 	}
 	else
 	{
-		// check in Theme music directory
 		if (!mCurrentThemeMusicDirectory.empty())
 			getMusicIn(mCurrentThemeMusicDirectory, musics);
 
-		// check in User music directory
 		if (musics.empty())
 			getMusicIn(Paths::getUserMusicPath(), musics);
 
-		// check in system sound directory
 		if (musics.empty())
 			getMusicIn(Paths::getMusicPath(), musics);
 
-		// check in .emulationstation/music directory
 		if (musics.empty())
 			getMusicIn(Paths::getUserEmulationStationPath() + "/music", musics);
 	}
@@ -249,19 +250,18 @@ void AudioManager::playRandomMusic(bool useFavorites)
 		return;
 
 	int randomIndex = Randomizer::random(musics.size());
-	while (songWasPlayedRecently(musics.at(randomIndex)))
+	while (songWasPlayedRecently(musics[randomIndex]))
 	{
-		LOG(LogDebug) << "Music \"" << musics.at(randomIndex) << "\" was played recently, trying again";
+		LOG(LogDebug) << "Music \"" << musics[randomIndex] << "\" was played recently, trying again";
 		randomIndex = Randomizer::random(musics.size());
 	}
 
-	// continue playing ?
-	if (mCurrentMusic != nullptr && useFavorites)
+	if (mCurrentMusic != nullptr && !useFavorites)
 		return;
 
-	playMusic(musics.at(randomIndex));
-	playSong(musics.at(randomIndex));
-	addLastPlayed(musics.at(randomIndex), musics.size());
+	playMusic(musics[randomIndex]);
+	playSong(musics[randomIndex]);
+	addLastPlayed(musics[randomIndex], musics.size());
 	mPlayingSystemThemeSong = "";
 }
 
